b6e7bdd6822e52e12c294c1e4454ce85c3ed39a1
Likith <likithsrinath2000@gmail.com>
2021-07-21 22:46:40 +0530

    Optimize code

--- app/src/main/java/in/bmsit/sixthsem/mean/AnalyseActivity.java
+++ app/src/main/java/in/bmsit/sixthsem/mean/AnalyseActivity.java
@@ -15,8 +15,6 @@ public class AnalyseActivity extends AppCompatActivity {
     TextView res;
     String[] data;
     String s1,s2,s3,s4;
-    String[] values = {"56","94","23","78","30"};
-    String[] titles = {"Hello", "My", "Name","Is","Deep"};
     String[] color = {"#dc143c", "#2ec571", "#8c7ae6", "#e84118", "#009432", "#487eb0", "#22a6b3", "#130f40"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,36 +27,18 @@ public class AnalyseActivity extends AppCompatActivity {
         s3 = s2.replace("}", "");
         s4 = s3.replace(":", ",");
         data = s4.split(",");
-
-//        for(int i=0; i<=7; i+=2)
-//        {
-//            res.append(data[i+1]);
-//            res.append("\n");
-//            res.append(data[i]);
-//            res.append("\n");
-//
-//        }
         drawPie();
-//        for (String str : data) {
-//            res.append(str);
-//            res.append("\n ");
-//        }
-
     }
-
 
     public void drawPie()
     {
         AnimatedPieView mAnimatedPieView = findViewById(R.id.pie);
         AnimatedPieViewConfig config = new AnimatedPieViewConfig();
         for(int i=0; i<=7; i+=2) {
-            config.startAngle(-90)// Starting angle offset
-                    .addData(new SimplePieInfo(Integer.parseInt(data[i+1]), Color.parseColor(color[i%5]), data[i]))//Data (bean that implements the IPieInfo interface)
-
+            config.startAngle(-90)
+                    .addData(new SimplePieInfo(Integer.parseInt(data[i+1]), Color.parseColor(color[i%5]), data[i]))
                     .drawText(true).duration(2000).textSize(30);
         }
-
-// The following two sentences can be replace directly 'mAnimatedPieView.start (config); '
         mAnimatedPieView.applyConfig(config);
         mAnimatedPieView.start();
     }
